9eecc7939f6516440a5de8a9104592050911ac85
SugarBoi23 <tuhienminh0@gmail.com>
2024-09-16 21:02:24 -0400

    ex2 test completed

--- ex2.py
+++ ex2.py
@@ -7,8 +7,7 @@ n_filter = water_quantity/5
 n_light = water_quantity/5*3
 kg_chlorine = water_quantity/10
 
-
-print(f"""Voici les éléments requis pour assainir {water_quantity}L d'eau:\n
-        \t- Filtre(s) : {n_filter}\n
-        \t- Lampe(s) UV : {n_light}\n
-        \t- Chlore : {kg_chlorine}kg\n""")
+print(f"""Voici les éléments requis pour assainir {water_quantity}L d'eau:
+\t- Filtre(s) : {n_filter:.0f}
+\t- Lampe(s) UV : {n_light:.0f}
+\t- Chlore : {kg_chlorine:.1f}kg""")
